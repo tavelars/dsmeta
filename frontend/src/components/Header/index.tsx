75ebd0e512bc60056e8ea4c80097fd9b3f0cdc44
tavelars <tavelars@gmail.com>
2022-11-17 16:04:34 -0300

    Reconfiguração do layout da tela

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/thiago-avelar-0a636421/">Thiago Avelar</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
+
+
+
