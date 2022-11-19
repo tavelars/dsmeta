ae8e9803e3e1ba984c34ced83cf15fb3127c53f0
tavelars <tavelars@gmail.com>
2022-11-19 10:55:57 -0300

    Configuração do envio de mensagem formatada de venda.

--- backend/src/main/java/com/devsuperior/dsmeta/services/SMSService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SMSService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dsmeta.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsmeta.entity.Sale;
+import com.devsuperior.dsmeta.repositories.SalesRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -22,15 +25,24 @@ public class SMSService {
 	@Value("${twilio.phone.to}")
 	private String twilioPhoneTo;
 
-	public void sendSms() {
+	@Autowired
+	private SalesRepository salesRepository;
+	
+	public void sendSms(Long id) {
 
+		Sale sale = salesRepository.findById(id).get();
+		String dataVenda = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		String msgTwilio = "O Vendedor " + sale.getSellerName() + " foi destaque em: " + dataVenda
+				+ " com um total de: " + String.format("%.2f", sale.getAmount());
+		
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		Message message = Message.creator(to, from, "Teste").create();
+		Message message = Message.creator(to, from, msgTwilio).create();
 
 		System.out.println(message.getSid());
+		
 	}
 }
